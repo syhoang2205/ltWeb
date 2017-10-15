9940d55f893378fedd35f6496f26272c91866f03
syhoang2205 <syhoang2205@gmail.com>
2017-10-15 20:06:29 +0700

    offical

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8887
--- Generation Time: Oct 15, 2017 at 08:03 AM
+-- Generation Time: Oct 15, 2017 at 01:03 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.5
 
@@ -40,8 +40,10 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `fullname`, `email`, `password`) VALUES
-(0, 'lê hoàng', 'hoang123@gmail.com', '$2y$10$q3vYxCyrfHoF5Bsn/gwNAeG2Q6Urpz5Bn/V8ghz440QcIlgCmvDz.'),
-(1, 'Lê S? Hoàng', 'syhoang22@gmail.com', '$2y$10$JYn7xUCCs5MRMTiG0To3jOkAoSj9dRF0pzGlT.ZGJRrpZl1e1f.le');
+(1, 'Lê S? Hoàng', 'syhoang22@gmail.com', '$2y$10$JYn7xUCCs5MRMTiG0To3jOkAoSj9dRF0pzGlT.ZGJRrpZl1e1f.le'),
+(5, 'lê hoàng', 'hoang123@gmail.com', '$2y$10$q3vYxCyrfHoF5Bsn/gwNAeG2Q6Urpz5Bn/V8ghz440QcIlgCmvDz.'),
+(6, '123', '123@gmail.com', '$2y$10$yPOceLxN92SuOL8RXqE6G.ubAYVxa3qZK4rP0XJ1X2bZiVqMGEyAe'),
+(9, '321', '123321@gmail.com', '$2y$10$ILTMbua/5f1v2Iic4exH9Onp4Exfu.3wtjA9aZsVnI61Rs67L6mo.');
 
 --
 -- Indexes for dumped tables
@@ -53,7 +55,16 @@ INSERT INTO `account` (`id`, `fullname`, `email`, `password`) VALUES
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `uniqueEmail` (`email`);
-COMMIT;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
